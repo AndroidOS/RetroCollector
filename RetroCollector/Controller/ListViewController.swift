a920109c5af50c151ee7376592e18dbadae9fa4d
AndroidOS <burrowsgit@gmail.com>
2020-08-15 07:50:36 +1000

    Added code for share button

--- RetroCollector/Controller/ListViewController.swift
+++ RetroCollector/Controller/ListViewController.swift
@@ -45,8 +45,27 @@ class ListViewController: UIViewController, UITableViewDelegate, FirebaseDataMan
     
 
     @IBAction func btnShare(_ sender: UIBarButtonItem) {
-        print("Share button pressed")
+        var text = ""
+        
+        for s in parts2 {
+            text.append(contentsOf: "\(s)\n")
+        }
+        
+        //let text = "This is some text that I want to share."
+
+               // set up activity view controller
+               let textToShare = [ text ]
+               let activityViewController = UIActivityViewController(activityItems: textToShare, applicationActivities: nil)
+               activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
+//
+//               // exclude some activity types from the list (optional)
+//        activityViewController.excludedActivityTypes = [ UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToFacebook ]
+
+               // present the view controller
+               self.present(activityViewController, animated: true, completion: nil)
     }
+    
+    
     @IBAction func btnLogout(_ sender: UIBarButtonItem) {
     }
     
